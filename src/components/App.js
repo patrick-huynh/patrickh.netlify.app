e9afdd97ee38511bfb96927fb5c3551c38903200
Patrick Huynh <patrick_huynh@cooperators.ca>
2021-11-10 21:08:41 -0500

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        console.log('tester');
+        return (
+            <div id="App">hello world</div>
+        );
+    }
+}
+
+export default App;
